d00c9f2f1860952f95eeba815d7ad47d8c09eecd
solne4niy <solne4niy88@yandex.ru>
2024-02-14 22:39:57 +0300

    Задание вроде это 4 было в листке домашних заданий

---
+++ src/study/HomeWorkOne.java
@@ -0,0 +1,15 @@
+package study;
+
+public class HomeWorkOne {
+    static void number(int a, int b){
+        if (a+b >= 10 && a+b <=20) {
+            System.out.println(true);
+        } else {
+            System.out.println(false);
+        }
+    }
+    public static void main(String[] args) {
+        number(10,99);
+    }
+}
+}
